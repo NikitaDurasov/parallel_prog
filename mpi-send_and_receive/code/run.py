0822bd402c006eec65a6aa76518134c1de883ad7
«NikitaDurasov» <yassnda@gmail.com>
2018-02-11 01:48:04 +0300

    add ping pong

--- mpi-send_and_receive/code/run.py
+++ mpi-send_and_receive/code/run.py
@@ -6,5 +6,7 @@ import subprocess
 sys_call = ''
 if sys.argv[1] == 'send_recv':
     sys_call = 'mpirun -n 3 ./send_recv'
+if sys.argv[1] == 'ping_pong':
+    sys_call = 'mpirun -n 2 ./ping_pong'
 print(sys_call)
 subprocess.call([sys_call], shell=True)
